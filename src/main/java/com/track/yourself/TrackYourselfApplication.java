883164fb5d73296330024134219bfe2f2aefd220
gingerose <72962151+gingerose@users.noreply.github.com>
2023-04-20 18:43:10 +0300

    Base project

---
+++ src/main/java/com/track/yourself/TrackYourselfApplication.java
@@ -0,0 +1,13 @@
+package com.track.yourself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackYourselfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackYourselfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/track/yourself/TrackYourselfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.track.yourself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackYourselfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
